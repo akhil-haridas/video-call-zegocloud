812558a2d63090e97943890d20040c08aa989b31
akhil-haridas <4khilharidas@gmail.com>
2024-10-04 19:00:40 +0530

    video config using the zego cloud

--- src/pages/RoomPage.jsx
+++ src/pages/RoomPage.jsx
@@ -1,9 +1,37 @@
+import { ZegoUIKitPrebuilt } from "@zegocloud/zego-uikit-prebuilt";
 import React from "react";
 import { useParams } from "react-router-dom";
 
 const RoomPage = () => {
-  const { roomId } = useParams();
-  return <div>RoomPage{roomId}</div>;
+    const { roomId } = useParams();
+
+    const myMeeting = async (element) => {
+        const appID = 531602696;
+        const serverSecret = "0befe57598ecff59254825270dd6ee0b";
+        const kitToken = ZegoUIKitPrebuilt.generateKitTokenForTest(
+            appID,
+            serverSecret,
+            roomId,
+            Date.now().toString(),
+            "Akhil K"
+        );
+        const zc = ZegoUIKitPrebuilt.create(kitToken);
+        zc.joinRoom({
+            container: element,
+            sharedLinks: [
+                { name: "Copy Link", url: `http://localhost:3001/room/${roomId}` },
+            ],
+            scenario: {
+                mode: ZegoUIKitPrebuilt.OneONoneCall,
+            },
+            showScreenSharingButton: false,
+        });
+    };
+    return (
+        <div>
+            <div ref={myMeeting} />
+        </div>
+    );
 };
 
 export default RoomPage;
