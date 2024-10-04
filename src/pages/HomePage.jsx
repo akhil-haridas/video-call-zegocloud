541625530415601971cbae0701d73f5885503d43
akhil-haridas <4khilharidas@gmail.com>
2024-10-04 18:29:19 +0530

    added join room button and input

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,9 +1,25 @@
-import React from 'react'
+import React, { useCallback, useState } from "react";
+import { useNavigate } from "react-router-dom";
 
 const HomePage = () => {
-    return (
-        <div>HomePage</div>
-    )
-}
+  const navigate = useNavigate();
+  const [value, setValue] = useState();
 
-export default HomePage
+  const onJoinRoom = useCallback(() => {
+    navigate(`/room${value}`);
+  }, [navigate, value]);
+
+  return (
+    <div>
+      <input
+        type="text"
+        placeholder="Enter Room Code"
+        value={value}
+        onChange={(e) => setValue(e.target.value)}
+      />
+      <button onClick={onJoinRoom}>Join</button>
+    </div>
+  );
+};
+
+export default HomePage;
